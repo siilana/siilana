3710cf03030711560e66f865fa408f2c7e4b38ee
siilana <hlimiimran1997@gmail.com>
2024-08-22 07:12:36 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 
 m.reply(`*قناتي على الواتساب:*\n
-*https://whatsapp.com/channel/0029VaLopUxFnSz7D2CH9S0P*
+https://whatsapp.com/channel/0029VaMEWK77dmeRpOolHG2f
 *تابعني هناك♥*`)
 }
 handler.help = ['channel']
